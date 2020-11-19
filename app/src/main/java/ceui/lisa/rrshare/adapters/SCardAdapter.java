0ff369530c93eca822d8150ae5ee61a6e57be575
CeuiLiSA <32817377+CeuiLiSA@users.noreply.github.com>
2020-11-19 17:09:40 +0800

    nonsense

--- app/src/main/java/ceui/lisa/rrshare/adapters/SCardAdapter.java
+++ app/src/main/java/ceui/lisa/rrshare/adapters/SCardAdapter.java
@@ -1,6 +1,8 @@
 package ceui.lisa.rrshare.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
@@ -8,6 +10,7 @@ import com.bumptech.glide.Glide;
 
 import java.util.List;
 
+import ceui.lisa.rrshare.MovieActivity;
 import ceui.lisa.rrshare.R;
 import ceui.lisa.rrshare.databinding.RecyScardBinding;
 import ceui.lisa.rrshare.response.Content;
@@ -16,6 +19,7 @@ public class SCardAdapter extends BaseAdapter<Content, RecyScardBinding> {
 
     public SCardAdapter(@Nullable List<Content> targetList, Context context) {
         super(targetList, context);
+        handleClick();
     }
 
     @Override
@@ -36,5 +40,16 @@ public class SCardAdapter extends BaseAdapter<Content, RecyScardBinding> {
         bindView.baseBind.score.setText(target.getScore());
         bindView.baseBind.tags.setText(target.getCat());
         Glide.with(mContext).load(target.getCover()).into(bindView.baseBind.imageView);
+    }
+
+    public void handleClick() {
+        setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position, int viewType) {
+                Intent intent = new Intent(mContext, MovieActivity.class);
+                intent.putExtra("content", allIllust.get(position));
+                mContext.startActivity(intent);
+            }
+        });
     }
 }
